f1631948e690b6544de739fc45425760a5c359c6
Toha Islam <islamtoha710@gmail.com>
2023-06-11 20:52:11 +0600

    split()

--- test.js
+++ test.js
@@ -22,7 +22,7 @@ let str2 = str.replace("Canada", "Bangladesh");
 
 // JavaScript String slice();
 let text = "Apple, Banana, Kiwi";
-let part = text.slice(7, 13);
+let part = text.slice(7, 13);    //slice(startingPoint,endPoint)
 // console.log(part);
 
 
@@ -45,4 +45,36 @@ let text2 = text1.toUpperCase();
 // JavaScript String toLowerCase()
 let text3 = "My name is Toha islam";
 let text4 = text1.toLowerCase();
-console.log(text4);
+// console.log(text4);
+
+
+// JavaScript String concat()
+// concat() joins two or more strings:
+let text5 = "Hello";
+let text6 = "World";
+let text7 = text6.concat(" ", text5);
+// console.log(text7);
+
+
+
+// The concat() method can be used instead of the plus operator. These two lines do the same:
+text = "Hello" + " " + "World!";
+text = "Hello".concat(" ", "World!");
+
+
+// Note
+// All string methods return a new string. They don't modify the original string.
+
+// Formally said:
+
+
+// Strings are immutable: Strings cannot be changed, only replaced
+
+
+
+// JavaScript String split()
+// A string can be converted to an array with the split() method:
+
+let emni = 'Toha Islam';
+const emni2 = emni.split("");
+console.log(emni2)
