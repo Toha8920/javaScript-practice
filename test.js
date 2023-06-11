4be37c22a4e0270165eb06082fcf56dab2ac1e29
Toha Islam <islamtoha710@gmail.com>
2023-06-11 19:06:50 +0600

    first commit

---
+++ test.js
@@ -0,0 +1,13 @@
+let car = {
+    name: 'ns',
+    color: 'black',
+    start: function () {
+        this.stop();
+        console.log('akash')
+    },
+    stop: function () {
+        console.log('toha')
+    }
+};
+
+console.log(car.start());
