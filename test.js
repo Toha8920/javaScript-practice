8f554c3934a986b9cd79c48161735eb054aea7f4
Toha Islam <islamtoha710@gmail.com>
2023-06-11 19:30:01 +0600

    substring

--- test.js
+++ test.js
@@ -14,7 +14,24 @@ let car = {
 // console.log(car.start());
 
 // javaScript replace function 
-
 let str = 'I love Canada';
 let str2 = str.replace("Canada", "Bangladesh");
-console.log(str2);
+// console.log(str2);
+
+
+
+// JavaScript String slice();
+let text = "Apple, Banana, Kiwi";
+let part = text.slice(7, 13);
+// console.log(part);
+
+
+
+// JavaScript String substring();
+// substring() is similar to slice().
+
+// The difference is that start and end values less than 0 are treated as 0 in substring();
+
+let fruit = "Apple, Banana, Kiwi";
+let fruits = str.substring(7, 13);
+console.log(fruits);
